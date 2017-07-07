73e9c73184fd0e331435292ce97fa2960c3c20e2
Osipava <helen30587@yandex.ru>
2017-07-07 23:45:42 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,9 +7,9 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.Period;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * GKislin
@@ -25,18 +25,23 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+       getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
         List<UserMealWithExceed> list = new ArrayList<>();
 
 
-        for(UserMeal userMeal : mealList)
-            if(TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime))
-                list.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), userMeal.getCalories()> caloriesPerDay));
+       mealList
+                .stream()
+               .forEach((UserMeal i) -> {
+                   if(TimeUtil.isBetween(i.getDateTime().toLocalTime(), startTime, endTime))
+                       list.add(new UserMealWithExceed(i.getDateTime(), i.getDescription(),i.getCalories(), i.getCalories() > caloriesPerDay));
+               });
+
         return list;
-    }
+    };
 }
